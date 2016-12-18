8e6bfc482c2fa4ec3e93f46cff3fea05f2ca1f48
Daniel Jung <daniel.jung92@googlemail.com>
2016-12-18 17:03:06 +0100

    implemented cubic spline

---
+++ inc/interpolation/IInterpolation.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../storage/Vec.h"
+
+namespace troll {
+namespace interpolation {
+	template <typename index, typename real>
+	class IInterpolation {
+	public:
+		virtual void Compute(storage::IVec<index, real>& x, storage::IVec<index, real>& y) = 0;
+
+		virtual real Eval(real x) = 0;
+		virtual storage::Vec<index, real> Eval(const storage::Vec<index, real>& x) = 0;
+	};
+}
+}
